61b2f4c40aadd9fb969e0347c61c130cff3f85c3
Eduardo Cardoso <eduardocardoso@outlook.com.br>
2017-12-06 00:00:50 -0200

    Adding Binary Search Tree header file

---
+++ binarytree.h
@@ -0,0 +1,23 @@
+/* Binary Search Tree prototype */
+#ifndef _BINTREE_H
+#define _BINTREE_H
+#include <stdbool.h>
+
+typedef struct tree {
+	int data;
+	struct tree *right, *left;
+} Tree;
+
+bool pushTo(Tree **root, int data);
+
+bool removeFrom(Tree **root, int data);
+
+bool search(Tree **root, int data);
+
+void preOrder(Tree *root);
+
+void inOrder(Tree *root);
+
+void postOrder(Tree *root);
+
+#endif /* binarytree.h */
